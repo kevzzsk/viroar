098388661d8d9b38669e09b8492dc478c4cab32e
KevinSk <kevi0023@e.ntu.edu.sg>
2020-09-30 01:13:46 +0800

    add 3d Model

---
+++ js/assets/dataModel.js
@@ -0,0 +1,108 @@
+module.exports = {
+    a: {
+        obj: require('./a/tinker.obj'),
+        mtl: require('./a/obj.mtl')
+    },
+    b: {
+        obj: require('./b/tinker.obj'),
+        mtl: require('./b/obj.mtl')
+    },
+    c: {
+        obj: require('./c/tinker.obj'),
+        mtl: require('./c/obj.mtl')
+    },
+    d: {
+        obj: require('./d/tinker.obj'),
+        mtl: require('./d/obj.mtl')
+    },
+    e: {
+        obj: require('./e/tinker.obj'),
+        mtl: require('./e/obj.mtl')
+    },
+    f: {
+        obj: require('./f/tinker.obj'),
+        mtl: require('./f/obj.mtl')
+    },
+    h: {
+        obj: require('./h/tinker.obj'),
+        mtl: require('./h/obj.mtl')
+    },
+    h: {
+        obj: require('./h/tinker.obj'),
+        mtl: require('./h/obj.mtl')
+    },
+    i: {
+        obj: require('./i/tinker.obj'),
+        mtl: require('./i/obj.mtl')
+    },
+    j: {
+        obj: require('./j/tinker.obj'),
+        mtl: require('./j/obj.mtl')
+    },
+
+    k: {
+        obj: require('./k/tinker.obj'),
+        mtl: require('./k/obj.mtl')
+    },
+    l: {
+        obj: require('./l/tinker.obj'),
+        mtl: require('./l/obj.mtl')
+    },
+    m: {
+        obj: require('./m/tinker.obj'),
+        mtl: require('./m/obj.mtl')
+    },
+    n: {
+        obj: require('./n/tinker.obj'),
+        mtl: require('./n/obj.mtl')
+    },
+    o: {
+        obj: require('./o/tinker.obj'),
+        mtl: require('./o/obj.mtl')
+    },
+    p: {
+        obj: require('./p/tinker.obj'),
+        mtl: require('./p/obj.mtl')
+    },
+    q: {
+        obj: require('./q/tinker.obj'),
+        mtl: require('./q/obj.mtl')
+    },
+    r: {
+        obj: require('./r/tinker.obj'),
+        mtl: require('./r/obj.mtl')
+    },
+    s: {
+        obj: require('./s/tinker.obj'),
+        mtl: require('./s/obj.mtl')
+    },
+    t: {
+        obj: require('./t/tinker.obj'),
+        mtl: require('./t/obj.mtl')
+    },
+    u: {
+        obj: require('./u/tinker.obj'),
+        mtl: require('./u/obj.mtl')
+    },
+    v: {
+        obj: require('./v/tinker.obj'),
+        mtl: require('./v/obj.mtl')
+    },
+    w: {
+        obj: require('./w/tinker.obj'),
+        mtl: require('./w/obj.mtl')
+    },
+    x: {
+        obj: require('./x/tinker.obj'),
+        mtl: require('./x/obj.mtl')
+    },
+    y: {
+        obj: require('./y/tinker.obj'),
+        mtl: require('./y/obj.mtl')
+    },
+    z: {
+        obj: require('./z/tinker.obj'),
+        mtl: require('./z/obj.mtl')
+    },
+
+}
